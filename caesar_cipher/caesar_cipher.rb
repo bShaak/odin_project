7ad32e2cffe9f9c634e61ac73ea5df0ab2a4c2cc
bShaak <bradenshaak@gmail.com>
2014-03-28 01:16:33 -0700

    removed magic numbers

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -2,6 +2,10 @@
 ascii capital letters 		65-90(decimal)
 ascii lower case letters 	97-122
 =end
+@lower_min = 97
+@lower_max = 122
+@upper_min = 65
+@upper_max = 90
 
 def caesar_cipher(text, shift)
 	if !shift.between?(0,25)
@@ -15,32 +19,31 @@ def caesar_cipher(text, shift)
 
 		tmp = j.ord
 		#upper case ascii
-		if tmp >= 65 && tmp <= 85
+		if tmp >= @upper_min && tmp <= (@upper_max - shift)
 			tmp += shift
 
 		#lower case ascii
-		elsif tmp >= 97 && tmp <= 117
+		elsif tmp >= @lower_min && tmp <= (@lower_max - shift)
 			tmp += shift
 
 		#wrap around upper case			
-		elsif tmp >= 86 && tmp <= 90
-			tmp = 65 + tmp + shift - 90
+		elsif tmp >= (@upper_max - (shift-1)) && tmp <= @upper_max
+			tmp = tmp + upper_min + shift - @upper_max
 
 		#wrap around lower case
-		elsif tmp >= 118 && tmp <= 122
-			tmp = 97 + tmp + shift - 122
+		elsif tmp >= (@lower_max - (shift-1)) && tmp <= @lower_max
+			tmp = tmp + @upper_min + shift - @upper_max
 		end
 
 		shifted_text += tmp.chr
 	end
 
-	#puts shifted_text
 	return shifted_text
 end
 
 txt = caesar_cipher("all lower case", 3)
 puts txt
-txt = caesar_cipher("Some UppEr", 5)
+txt = caesar_cipher("Some UppErz", 5)
 puts txt
 txt = caesar_cipher("Shift too big", 27)
 puts txt
